cb955c7294ba518dd57a9e9877541ee2359c962e
NMrocks <nmrocks2828@gmail.com>
2022-01-08 17:27:12 +0530

    Add multiple files, write code for program
    The core functionality of the program is ready, and I've locally placed my urban++ library and srilakshmikanthanp's figlet library and figlet font

---
+++ src/include/urban++.hpp
@@ -0,0 +1,168 @@
+/*
+
+    Copyright (c) 2021, 2022 Nishant Mishra <https://github.com/NMrocks>
+
+    This software is released under the MIT License.
+    See https://choosealicense.com/licenses/mit/ for more info.
+
+*/
+
+/*
+This program works by fetching the JSON from the Urban Dictionary API url at api.urbandictionary.com
+(https://dev.to/nhighleysalongenius/comment/epgk)
+I don't know how this works or when Urban dictionary might lock this API down, but you can use this lib
+until then :P
+*/
+
+#ifndef URBAN_PLUS_PLUS
+#define URBAN_PLUS_PLUS
+
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+
+namespace nm
+{
+    class Urban
+    {
+        private:
+        std::string searchTerm, searchResult;
+        std::string randomURL = "https://api.urbandictionary.com/v0/random";
+        std::string urlPrefix = "https://api.urbandictionary.com/v0/define?term=";
+        int sizeOfList;
+
+        CURL *curl;
+        CURLcode res;
+        nlohmann::json resultJSON;
+
+        static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) // Method to write URL JSON data to std::string
+        {
+            ((std::string*)userp)->append((char*)contents, size * nmemb);
+            return size * nmemb;
+        }
+
+        public:
+        Urban() // Sets all curl options
+        {
+            curl = curl_easy_init();
+            if (curl) {
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &searchResult);
+                curl_easy_setopt(curl, CURLOPT_TIMEOUT, 30);
+            }
+            else {
+                std::cerr << "Fatal error: Unable to initialize curl object.";
+                exit(1);
+            }
+        }
+        ~Urban()
+        {
+            curl_easy_cleanup(curl);
+        }
+
+        void setSearchTerm(const std::string term); // Sets the search term
+        void setSearchTerm(const char *term); // Overloaded method for char * type
+
+        CURLcode fetch(); // Fetch results and return CURLcode for success/any errors
+        CURLcode fetchRandom(); // Fetch results for a random word
+
+        /*
+        The format for getters is: get<top/bottom/><property>(unsigned int index as argument in case of non-top/bottom getter)
+        
+        getTop<property>() returns the specific property (for example definition) of the top result in the array of definitions
+        for the current word from the list of results in the JSON
+        Same for the getBottom<property>(), except that it returns the bottom most result.
+
+        get<property>(unsigned int index) returns the property of the result at index <index>
+
+        The type returned depends on the property, for example it is std::string for getDefinition(index) but it is
+        std::uint64_t for getThumbsUb(index)
+        */
+
+        std::string getDefinition(unsigned int index) { return resultJSON["list"][index]["definition"]; };
+        std::string getTopDefinition() { return getDefinition(0); }
+        std::string getBottomDefinition() { return getDefinition(sizeOfList - 1); }
+
+        std::uint64_t getThumbsUp(unsigned int index) { return resultJSON["list"][index]["thumbs_up"]; };
+        std::uint64_t getTopThumbsUp() { return getThumbsUp(0); }
+        std::uint64_t getBottomThumbsUp() { return getThumbsUp(sizeOfList - 1); }
+        std::uint64_t getThumbsDown(unsigned int index) { return resultJSON["list"][index]["thumbs_down"]; };
+        std::uint64_t getTopThumbsDown() { return getThumbsDown(0); }
+        std::uint64_t getBottomThumbsDown() { return getThumbsDown(sizeOfList - 1); }
+
+        std::string getPermalink(unsigned int index) { return resultJSON["list"][index]["permalink"]; };
+        std::string getTopPermalink() { return getPermalink(0); };
+        std::string getBottomPermalink() { return getPermalink(sizeOfList - 1); };
+
+        std::vector <std::string> getSoundURLs(unsigned int index) { return resultJSON["list"][index]["sound_urls"]; };
+        std::vector <std::string> getTopSoundURLs() { return getSoundURLs(0); };
+        std::vector <std::string> getBottomSoundURLs() { return getSoundURLs(sizeOfList - 1); };
+        
+        std::string getAuthor(unsigned int index) { return resultJSON["list"][index]["author"]; };
+        std::string getTopAuthor() { return getAuthor(0); };
+        std::string getBottomAuthor() { return getAuthor(sizeOfList - 1); };
+
+        std::string getWord(unsigned int index) { return resultJSON["list"][index]["word"]; };
+        std::string getTopWord() { return getWord(0); };
+        std::string getBottomWord() { return getWord(sizeOfList - 1); };
+
+        std::uint64_t getDefID(unsigned int index) { return resultJSON["list"][index]["defid"]; };
+        std::uint64_t getTopDefID() { return getDefID(0); };
+        std::uint64_t getBottomDefID() { return getDefID(sizeOfList - 1); };
+
+        std::string getWrittenOn(unsigned int index) { return resultJSON["list"][index]["written_on"]; };
+        std::string getTopWrittenOn() { return getWrittenOn(0); };
+        std::string getBottomWrittenOn() { return getWrittenOn(sizeOfList - 1); };
+
+        std::string getExample(unsigned int index) { return resultJSON["list"][index]["example"]; };
+        std::string getTopExample() { return getExample(0); };
+        std::string getBottomExample() { return getExample(sizeOfList - 1); }
+    };
+
+    void Urban::setSearchTerm(const std::string term)
+    {
+        searchTerm = std::regex_replace(term, std::regex(" "), "%20");
+        curl_easy_setopt(curl, CURLOPT_URL, (urlPrefix + searchTerm).data());
+    }
+    
+    void Urban::setSearchTerm(const char *term)
+    {
+        searchTerm = std::regex_replace(term, std::regex(" "), "%20");
+        curl_easy_setopt(curl, CURLOPT_URL, (urlPrefix + searchTerm).data());
+    }
+
+    CURLcode Urban::fetch()
+    {
+        res = curl_easy_perform(curl);
+        if (res == CURLE_OK) {
+            resultJSON = nlohmann::json::parse(searchResult); // Parse JSON result string and save to resultJSON
+            sizeOfList = resultJSON["list"].size(); // Set sizeOfList to the length of "list" vector in JSON
+            if (sizeOfList == 0) return CURLE_GOT_NOTHING; // Return CURLE_GOT_NOTHING if the word does not have any search results
+            searchResult = ""; // Resets the search result string
+        }
+        return res;
+    }
+
+    CURLcode Urban::fetchRandom()
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, randomURL.data()); // Sets the URL to the random URL
+        
+        res = curl_easy_perform(curl);
+        if (res == CURLE_OK) {
+            resultJSON = nlohmann::json::parse(searchResult);
+            sizeOfList = resultJSON["list"].size();
+            searchResult = "";
+        }
+
+        curl_easy_setopt(curl, CURLOPT_URL, (urlPrefix + searchTerm).data()); // Restores the original search term URL
+        return res;
+    }
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -17,3 +17,57 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
+
+#include <iostream>
+#include <cstdlib>
+
+#include <curl/curl.h>
+#include <string>
+#include "include/Figlet.hpp"
+#include "include/urban++.hpp"
+
+using srilakshmikanthanp::FigletFont;
+using srilakshmikanthanp::Smushed;
+
+std::string repeat(int n, std::string str) {
+    std::ostringstream os;
+    for(int i = 0; i < n; i++)
+        os << str;
+    return os.str();
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        std::cerr << "Error: No word/phrase provided";
+        return 1;
+    }
+
+    srilakshmikanthanp::Figlet figlet(FigletFont::make("figlet-font/Standard.flf"), Smushed::make());
+    nm::Urban urban;
+
+    urban.setSearchTerm(argv[1]);
+    CURLcode err = urban.fetch();
+    
+    double likeDislikeRatio = 0;
+
+    if (err == CURLE_OK) {
+        if (urban.getTopThumbsDown() != 0) likeDislikeRatio = (double) urban.getTopThumbsUp()/urban.getTopThumbsDown();
+
+        std::cout << figlet(urban.getWord(0));
+
+        std::cout << urban.getTopDefinition() << "\n\n";
+        std::cout << urban.getTopExample() << "\n\n";
+        std::cout << "by " << urban.getTopAuthor() << "\n";
+        std::cout << "👍 " << urban.getTopThumbsUp() << "    👎 " << urban.getTopThumbsDown() << "    👍/👎 " << likeDislikeRatio << "\n";
+        
+        std::cout << repeat(80, "─") << std::endl;
+        
+        return 0;
+    }
+    if (err == CURLE_GOT_NOTHING) {
+        std::cerr << "No search results found for word/phrase \"" << argv[1] << "\"\n";
+        return 1;
+    }
+    else std::cerr << "An error occured while fetching results: " <<curl_easy_strerror(err) << "\n";
+    return 1;
+}
