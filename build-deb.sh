b13a573c931535fe9a9e162130ae80d8a4b6f4bb
NMrocks <nmrocks2828@gmail.com>
2022-09-04 16:39:21 +0530

    Improve build-deb script
    The build-deb script now uses heredoc to save the deb control info to the control file instead of the older multiple echos

--- build-deb.sh
+++ build-deb.sh
@@ -4,7 +4,7 @@
 # successfully.
 
 echo "---Running build-deb.sh---"
-printf "Arch of compiled binary [amd64/armel/i386 etc]: "
+printf "Arch of compiled binary [amd64/arm64 etc]: "
 read arch
 version=$(bin/urbanite --dumpversion) # Version string
 size=$(du -ch -B "K" bin/urbanite fonts | tail -n1 | cut -d "K" -f 1) # Size variable for Installed-Size
@@ -17,22 +17,27 @@ echo "...done"
 
 # File 2: control.tar.xz
 echo "Creating control file..."
-echo "Package: urbanite" > deb-build/control
-echo "Package-Type: deb" >> deb-build/control
-echo "Version: " $version >> deb-build/control
-echo "Maintainer: Nishant Mishra <nmrocks2828@gmail.com>" >> deb-build/control
-echo "Description: Search words on Urban Dictionary at the comfort of your terminal" >> deb-build/control
-echo " Urbanite is a program for searching words on Urban Dictionary" >> deb-build/control
-echo " (https://urbandictionary.com) from within a terminal emulator/shell/tty." >> deb-build/control
-echo -e " .\n Urbanite features a Figlet-style word heading, customizable figlet fonts for" >> deb-build/control
-echo " the heading, definition stats (like/dislike counts and ratios) and more." >> deb-build/control
-echo "Section: utils" >> deb-build/control
-echo "Priority: optional" >> deb-build/control
-echo "Installed-Size: $size" >> deb-build/control
-echo "Architecture: $arch" >> deb-build/control
-echo "Bugs: https://github.com/NMrocks/urbanite/issues" >> deb-build/control
-echo "Depends: libcurl4" >> deb-build/control
-echo "Suggests: curl, figlet" >> deb-build/control
+
+cat << EOF > deb-build/control
+Package: urbanite
+Package-Type: deb
+Version: $version
+Maintainer: Nishant Mishra <nmrocks2828@gmail.com>
+Description: Search words on Urban Dictionary at the comfort of your terminal
+ Urbanite is a program for searching words on Urban Dictionary
+ (https://urbandictionary.com) from within a terminal emulator/shell/tty.
+ .
+ Urbanite features a Figlet-style word heading, customizable figlet fonts for
+ the heading, definition stats (like/dislike counts and ratios) and more.
+Section: utils
+Priority: optional
+Installed-Size: $size
+Architecture: $arch
+Bugs: https://github.com/NMrocks/urbanite/issues
+Depends: libcurl4
+Suggests: curl, figlet
+EOF
+
 echo "...done"
 
 # File 3: data.tar.xz (not archived here)
